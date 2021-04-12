1b0c56e4c59490e009f5ed2aecba1689d229f455
ridwan-transcon <it3@tciwarehouse-global.com>
2021-04-12 15:57:50 +0700

    progress

--- application/migrations/20210407000002_create_table_research_permits.php
+++ application/migrations/20210407000002_create_table_research_permits.php
@@ -2,18 +2,21 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 /**
- * Class Migration_Create_table_research_permits
+ * Class Migration_Create_table_ref_lecturer
  * @property CI_DB_forge $dbforge
  */
-class Migration_Create_table_research_permits extends CI_Migration
+class Migration_Create_table_ref_lecturer extends CI_Migration
 {
     public function up()
     {
         $this->dbforge->add_field([
             'id' => ['type' => 'INT', 'unsigned' => TRUE, 'constraint' => 11, 'auto_increment' => TRUE],
-            'no_lecturer' => ['type' => 'VARCHAR', 'constraint' => '50', 'unique' => TRUE],
-            'name' => ['type' => 'VARCHAR', 'constraint' => '50'],
-            'position' => ['type' => 'VARCHAR', 'constraint' => '50', 'null' => TRUE],
+            'id_kaprodi' => ['type' => 'INT', 'unsigned' => TRUE, 'constraint' => 11, 'null' => TRUE],
+            'id_pembimbing' => ['type' => 'INT', 'unsigned' => TRUE, 'constraint' => 11, 'null' => TRUE],
+            'nim' => ['type' => 'VARCHAR', 'constraint' => '50', 'unique' => TRUE],
+            'name' => ['type' => 'VARCHAR', 'constraint' => '100'],
+            'email' => ['type' => 'VARCHAR', 'constraint' => '100', 'null' => TRUE],
+            'terhormat' => ['type' => 'VARCHAR', 'constraint' => '100', 'null' => TRUE],
             'description' => ['type' => 'TEXT', 'null' => TRUE],
             'is_deleted' => ['type' => 'INT', 'constraint' => 1, 'default' => 0],
             'created_at' => ['type' => 'TIMESTAMP DEFAULT CURRENT_TIMESTAMP'],
@@ -22,15 +25,16 @@ class Migration_Create_table_research_permits extends CI_Migration
             'updated_by' => ['type' => 'INT', 'constraint' => 11, 'unsigned' => TRUE, 'null' => TRUE],
             'deleted_at' => ['type' => 'TIMESTAMP', 'null' => true],
             'deleted_by' => ['type' => 'INT', 'constraint' => 11, 'unsigned' => TRUE, 'null' => TRUE]
-        ]);
+        ])
+        ->add_field('CONSTRAINT fk_training_exercise_score_training FOREIGN KEY (id_training) REFERENCES trainings(id) ON DELETE CASCADE ON UPDATE CASCADE');
         $this->dbforge->add_key('id', TRUE);
-        $this->dbforge->create_table('research_permits');
-        echo 'Migrate Migration_Create_table_research_permits' . PHP_EOL;
+        $this->dbforge->create_table('ref_lecturer');
+        echo 'Migrate Migration_Create_table_ref_lecturer' . PHP_EOL;
     }
 
     public function down()
     {
-        $this->dbforge->drop_table('research_permits');
-        echo 'Rollback Migration_Create_table_research_permits' . PHP_EOL;
+        $this->dbforge->drop_table('ref_lecturer');
+        echo 'Rollback Migration_Create_table_ref_lecturer' . PHP_EOL;
     }
 }
