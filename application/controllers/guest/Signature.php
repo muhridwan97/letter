2f6e76ada0fe0c01ca00b2edb208bdae2870500c
Muhammad Ridwan <muh.ridwan97@gmail.com>
2021-06-03 19:04:17 +0700

    fiX SURAT TUGAS

--- application/controllers/guest/Signature.php
+++ application/controllers/guest/Signature.php
@@ -7,6 +7,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
  * @property LecturerModel $lecturer
  * @property NotificationModel $notification
  * @property ResearchPermitModel $researchPermit
+ * @property AssignmentLetterModel $assignmentLetter
  * @property Exporter $exporter
  * @property Uploader $uploader
  * @property Mailer $mailer
@@ -28,6 +29,7 @@ class Signature extends App_Controller
 		$this->load->model('modules/Mailer', 'mailer');
 		$this->load->model('notifications/CreateCourseNotification');
 		$this->load->model('ResearchPermitModel', 'researchPermit');
+		$this->load->model('AssignmentLetterModel', 'assignmentLetter');
 
 		$this->setFilterMethods([
 		]);
@@ -51,7 +53,13 @@ class Signature extends App_Controller
 				$data['tujuan'].='No Surat : '.$researchPermit['no_letter'].'<br>';
 				$data['signature_by'] = $lecturer['name'];
 				break;
-			
+			case SignatureModel::TYPE_ASSIGNMENT_LETTER:
+				$assignmentLetter = $this->assignmentLetter->getById($signatures['id_reference']);
+				$lecturer = $this->lecturer->getById($signatures['id_lecturer']);
+				$data['tujuan']='Surat : Permohonan Surat Tugas <br>';
+				$data['tujuan'].='No Surat : '.$assignmentLetter['no_letter'].'<br>';
+				$data['signature_by'] = $lecturer['name'];
+				break;
 			default:
 				# code...
 				break;
